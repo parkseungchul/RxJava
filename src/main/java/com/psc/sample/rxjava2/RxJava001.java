c3051520814c861847ff222453b9f045fbc171dd
simon <parkseungchul@gmail.com>
2021-06-24 23:35:55 +0900

    frist

--- src/main/java/com/psc/sample/rxjava2/RxJava001.java
+++ src/main/java/com/psc/sample/rxjava2/RxJava001.java
@@ -2,17 +2,82 @@ package com.psc.sample.rxjava2;
 
 import io.reactivex.Flowable;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 public class RxJava001 {
+
+    /**
+     * RxJava2 import
+     * Publisher -> Subscribe
+     * 생산 -> 소비
+     */
+    public static void A001(){
+        Flowable<Integer> flowable = Flowable.just(1,2,3,4,5,6,7,8,9,10);
+        flowable.subscribe(data ->  System.out.print(data));
+        System.out.println();
+        flowable.subscribe(x -> System.out.print(x));
+        System.out.println();
+    }
+
+    /**
+     * RxJava2 import
+     * Publisher -> Subscribe
+     * 생산 -> 가공,소비
+     */
+    public static void A002() throws InterruptedException {
+        Flowable<Integer> flowable = Flowable.just(1,2,3,4,5,6,7,8,9,10);
+        // 조건
+        flowable.filter(x -> (x%2==0)).subscribe(x -> System.out.print(x));
+        System.out.println();
+        // 갯수
+        flowable.filter(x -> (x%2==0)).take(2).subscribe(x -> System.out.print(x));
+        System.out.println();
+
+        // 첫번째
+        flowable.filter(x -> (x%2==0)).first(1).subscribe(x -> System.out.print(x));
+        System.out.println();
+        // 첫번째 없을때 디폴트 값
+        flowable.filter(x -> (x%100==0)).first(100).subscribe(x -> System.out.print(x));
+    }
+
+
+    /**
+     * 비동기임
+     * 비동기 처리에는 생산과 소비처리가 외부에 노출되는 잣되는 경우
+     */
+    private enum TypeCal{
+        ADD,SUBTRACTION;
+    }
+    private static TypeCal typeCal;
+    public static void A003() throws InterruptedException {
+
+        typeCal = TypeCal.ADD;
+        Flowable<Long> flowable = Flowable.interval(300L, TimeUnit.MILLISECONDS)
+                .take(10).scan((sum, data) ->{
+                    if(typeCal == TypeCal.ADD){
+                        sum = sum + data;
+                    }else{
+                        sum = sum - data;
+                    }
+           return sum;
+        });
+        flowable.subscribe(data ->  System.out.println("data["+ typeCal +"]=" + data));
+        Thread.sleep(900);
+        typeCal = TypeCal.SUBTRACTION;
+        Thread.sleep(5000);
+
+    }
 
     public static void main(String[] args) throws InterruptedException {
 
-        // Case1: 생산 소비 바로
-        // 생산자
-        Flowable<String> flowable1 = Flowable.just("A", "B","C");
-        // 소비자
-        flowable1.subscribe(data -> { System.out.print(data);  } );
-        System.out.println();
+        //A001();
+        //A002();
+        //A003();
 
+
+/**
         // Case2: 생산 데이터 가공 두 번 후출 값 같음
         Flowable<Integer> flowable2 = Flowable
                 .just(1,2,3,4,5,6,7)
@@ -23,7 +88,7 @@ public class RxJava001 {
         flowable2.subscribe(data -> System.out.print(data));
         System.out.println();
 
-        // Case3. fromCallable 를 사용할 경우 호출할때 ㄱ
+        // Case3. fromCallable 를 사용할 경우 호출할때
         Flowable<Long> flowable3 = Flowable.just(System.currentTimeMillis());
         System.out.println(flowable3.subscribe(data -> System.out.println(data)));
         Thread.sleep(1000);
@@ -46,5 +111,8 @@ public class RxJava001 {
                             return  j;
                         });
         flowable4.subscribe(x -> System.out.println(x));
-    }
+ **/
+        }
+
+
 }
