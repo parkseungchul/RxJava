dee49247281271045310b7a05dc1acd0526d2c6f
seungchulpark <parkseungchul@gmail.com>
2021-09-29 13:19:38 +0900

    update

--- demo/src/main/java/com/example/demo/controller/DeptController.java
+++ demo/src/main/java/com/example/demo/controller/DeptController.java
@@ -9,6 +9,8 @@ import org.springframework.web.reactive.result.view.Rendering;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.stream.Collectors;
+
 @Slf4j
 @Controller
 public class DeptController {
@@ -21,6 +23,23 @@ public class DeptController {
         log.debug("index");
         return Mono.just(Rendering.view("index.html")
                 .modelAttribute("depts",  this.deptRepository.findAll())
+                .build());
+    }
+
+    @GetMapping("/search/sal")
+    public Mono<Rendering> search(Integer sal){
+
+        return Mono.just(Rendering.view("search.html")
+                .modelAttribute("depts",
+                        this.deptRepository.findAll().flatMap(dept -> {
+                            if(dept.getEmpList() != null){
+                                dept.setEmpList(dept.getEmpList().stream().filter(emp -> emp.getSal() > (sal!=null?sal:0)).collect(Collectors.toList()));
+                                return Flux.just(dept);
+                            }else{
+                                return Flux.empty();
+                            }
+                        })
+                )
                 .build());
     }
 
